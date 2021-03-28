cc07d6b902586695920afa0086cf70bb0460eb94
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-28 14:33:13 -0700

    Creates base project structure

---
+++ CovidApp/CovidApp/CovidAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidAppApp.swift
+//  CovidApp
+//
+//  Created by user189351 on 3/28/21.
+//
+
+import SwiftUI
+
+@main
+struct CovidAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ListView()
+        }
+    }
+}
---
+++ CovidApp/CovidApp/ListView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CovidApp
+//
+//  Created by Enrique Diaz de Leon Hicks on 3/28/21.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
