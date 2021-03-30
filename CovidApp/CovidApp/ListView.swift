c8db83d443d01f46f26f189dc92a7cb8f7a8c2a1
enriquedlh97 <enriquedlh97@hotmail.com>
2021-03-29 18:21:06 -0700

    Added fucntionality in ListView to show all countries in a list

--- CovidApp/CovidApp/ListView.swift
+++ CovidApp/CovidApp/ListView.swift
@@ -8,13 +8,25 @@
 import SwiftUI
 
 struct ListView: View {
+    
+    @StateObject var cases = DataModel() // wrapper is used to create the reference in this view to the data model and keep it alive for use in the view and other views.
+    // Essentially with the wrapper we can use the variable in various viwes without
+    // having to create it externally to SwitUI and inject it in.
+    // It makes sure the object doe snot get destroyed when the view updates
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                List(cases.casesList) { country in // iterate over country list which is casesList
+                    Text(country.country)
+                }
+            }
+        }
     }
 }
 
-struct ListbbbbbView_Previews: PreviewProvider {
+struct ListView_Previews: PreviewProvider {
     static var previews: some View {
         ListView()
     }
